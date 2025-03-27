ee1fc64eedb59377b52f2862cda03426f6b7dc7d
Vineet Panchal <vineetpanchal03@gmail.com>
2025-03-27 17:57:57 -0400

    oops

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyACtXOGgInug-Pw0DPrNSjxkulxdhvoHNc",
-  authDomain: "smartdeck-draft.firebaseapp.com",
-  projectId: "smartdeck-draft",
-  storageBucket: "smartdeck-draft.firebasestorage.app",
-  messagingSenderId: "438446216141",
-  appId: "1:438446216141:web:6a19620c789504809cea84",
-  measurementId: "G-0SGN9MTP4G"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
